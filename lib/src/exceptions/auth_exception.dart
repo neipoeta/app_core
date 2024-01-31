1552f0e14c1be4b73ee4ffa03564c7bd432871e1
neipoeta <neipoeta@outlook.com>
2024-01-31 01:25:05 -0300

    exceptions

---
+++ lib/src/exceptions/auth_exception.dart
@@ -0,0 +1,13 @@
+sealed class AuthException {
+  final String message;
+
+  AuthException({required this.message});
+}
+
+final class AuthError extends AuthException {
+  AuthError({required super.message});
+}
+
+final class AuthUnauthorizedException extends AuthException {
+  AuthUnauthorizedException() : super(message: 'Unauthorized');
+}
